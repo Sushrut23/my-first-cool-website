af922cd08cc71119348cea51f972a39863384e16
Sushrut M <sushrut.mirzapure@innovaccer.com>
2025-05-17 20:46:38 +0530

    deployy updates

--- grocery-app/server/src/routes/groceryRoutes.ts
+++ grocery-app/server/src/routes/groceryRoutes.ts
@@ -4,7 +4,19 @@ import { GroceryController } from '../controllers/groceryController';
 const router = Router();
 const controller = new GroceryController();
 
-router.post('/add-item', (req, res) => controller.addItem(req, res));
-router.get('/items-by-date', (req, res) => controller.getItemsByDate(req, res));
+router.post('/add-item', async (req, res, next) => {
+  try {
+    await controller.addItem(req, res);
+  } catch (err) {
+    next(err);
+  }
+});
+router.get('/items-by-date', async (req, res, next) => {
+  try {
+    await controller.getItemsByDate(req, res);
+  } catch (err) {
+    next(err);
+  }
+});
 
 export default router;
